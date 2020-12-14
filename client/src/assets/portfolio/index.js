060d8e0a68295eaeb34e77c1375f31eea2cba1f2
DebabrataPal <dpal@noon.com>
2020-12-14 23:18:43 +0530

    assets

--- client/src/assets/portfolio/index.js
+++ client/src/assets/portfolio/index.js
@@ -1,10 +1,14 @@
 export const Portfolio = {
     portrait: {
-        1: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC2218-Recovered.jpg",
-        2: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC2941.JPG",
-        3: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC3028.JPG",
+        9: "https://avikassets.s3.ap-south-1.amazonaws.com/DSC04981.jpg",
+        8: "https://avikassets.s3.ap-south-1.amazonaws.com/DSC04962.jpg",
+        7: "https://avikassets.s3.ap-south-1.amazonaws.com/DSC04934.jpg",
+        6: "https://avikassets.s3.ap-south-1.amazonaws.com/DSC04905.jpg",
+        5: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC3606.jpg",
         4: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC3035.JPG",
-        5: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC3606.jpg"
+        3: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC3028.JPG",
+        1: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC2218-Recovered.jpg",
+        2: "https://avikassets.s3.ap-south-1.amazonaws.com/_DSC2941.JPG"
     },
     landscape: {
         1: require("./landscape1.jpeg"),
